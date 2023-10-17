74ed2854d07627f1aca40ce48fa7b9fb575174e5
Muwavictoria <victoriamuwanguzi!9@gmail.com>
2023-10-17 14:03:56 +0300

    CAR BOOKING first commit flask

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from  flask import Flask
+from flask_mysqldb import MySQL
+
+
+
+app = Flask(__name__)
+
+app.secret_key = 'your secret key'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'car_app'
+
+
+mysql = MySQL(app)
+
+from app import routes
+
+  
